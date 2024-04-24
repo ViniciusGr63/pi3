c52f6ba50891726b8c637fe134e10d5fb18f27be
student-lab03 <>
2024-04-24 20:21:00 -0300

    backend

---
+++ src/test/java/com/example/pi3/Pi3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pi3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
