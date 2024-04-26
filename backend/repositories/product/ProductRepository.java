0d352e59d8e04451401ed08ab5d75baf51fca07f
ViniciusGr63 <128187154+ViniciusGr63@users.noreply.github.com>
2024-04-26 18:34:40 +0000

    criando estrutura mvc e 2 classes modelos

---
+++ backend/repositories/product/ProductRepository.java
@@ -0,0 +1,20 @@
+public class ProductRepository{
+    Product productItem = new Product();
+
+
+
+
+
+    public bool createProduct(productItem exemple){
+        return true;
+    }
+    public bool readProduct(){
+        return true;
+    }
+    public bool updateProduct(){
+        return true;
+    }
+    public bool deleteProduct(){
+        return true;
+    }
+}
