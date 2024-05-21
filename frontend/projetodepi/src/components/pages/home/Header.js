27bdd5cb8538c4671d96f337978827a1df50d49b
ViniciusGr63 <128187154+ViniciusGr63@users.noreply.github.com>
2024-05-21 22:18:00 +0000

    enviando homepage

--- frontend/projetodepi/src/components/pages/home/Header.js
+++ frontend/projetodepi/src/components/pages/home/Header.js
@@ -3,11 +3,14 @@
 import styles from './Nav.module.css';
 import DropdownMenu from './../../routes/MenuHome';
 import { Link } from 'react-router-dom';
+import LogoC from './../../../assets/images/LOGOCOCON.png'
 
 function Header (){
     return(
         <>
         <header className={styles.HeaderStyle}>
+        
+       <img src={LogoC} alt="Logo_ComCom" />
         <nav className={styles.nav}>         
         <ul className={styles.navEstilo}>
         <li className={styles.item}><Link to="/">Home</Link></li>
